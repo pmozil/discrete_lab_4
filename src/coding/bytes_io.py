4d6ce526d361c51213b8a1d7723a30f5cef4bb78
Petro Mozil <mozil.petryk@gmail.com>
2023-03-15 18:58:24 +0200

    Update bytes_io

--- src/coding/bytes_io.py
+++ src/coding/bytes_io.py
@@ -31,7 +31,15 @@ class BytesIO:
             pickle.dump(alp, out)
 
     @staticmethod
-    def readt_from_file(fname: str) -> list[tuple[int, int]]:
+    def read_dict_from_file(fname: str) -> dict[bytes, Any]:
+        """
+        Yeah, i made this a different file, AND with pickle. so what?
+        """
+        with open(fname, "rb") as out:
+            return pickle.load(out)
+
+    @staticmethod
+    def read_data_from_file(fname: str) -> list[tuple[int, int]]:
         result = []
         with open(fname, "rb") as inp:
             while info := inp.read(130):
@@ -43,3 +51,24 @@ class BytesIO:
                 )
 
         return result
+
+    @staticmethod
+    def read_to_int_list(fname: str, byte_len: int = 1) -> list[int]:
+        result = []
+        with open(fname, "rb") as inp:
+            while info := inp.read(byte_len):
+                result.append(int.from_bytes(info, "little"))
+
+        return result
+
+    @staticmethod
+    def write_ints_to_file(fname: str, info: list[int], byte_len: int = 1):
+        """
+        Write a huffmann-encoded thing into a file. Why?
+        Probably because I hate myself
+        """
+        result = bytes()
+        with open(fname, "wb") as out:
+            for prefix in info:
+                result += prefix.to_bytes(byte_len, "little")
+            out.write(result)
